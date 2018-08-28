7f6f7857e110c5c6f0188bc5906686961b9ca373
Rob Hagemans <robhagemans@yahoo.co.uk>
2018-08-28 21:57:31 +0100

    bump version number to 2.1.dev0

--- pcbasic/metadata.py
+++ pcbasic/metadata.py
@@ -1,7 +1,7 @@
 
 NAME = u'PC-BASIC'
 PACKAGE = b'pcbasic'
-VERSION = u'2.0.0'
+VERSION = u'2.1.dev0'
 COPYRIGHT = u'(C) Copyright 2013--2018 Rob Hagemans.'
 LICENCE = u'GPLv3'
 AUTHOR = u'Rob Hagemans'
