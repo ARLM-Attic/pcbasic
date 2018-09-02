36eb3dab74a99f2ab71872723d0b1e696be7a4de
Rob Hagemans <robhagemans@yahoo.co.uk>
2018-09-02 19:34:45 +0100

    fix implicitly extending field buffer; better encapsulate fields

--- pcbasic/basic/base/bytestream.py
+++ pcbasic/basic/base/bytestream.py
@@ -10,11 +10,29 @@ from _pyio import BytesIO
 
 
 class ByteStream(BytesIO):
-    """Extension of BytesIO with accessible buffer."""
+    """BytesIO over external buffer."""
 
     def __init__(self, initial_bytes):
         """Create new ByteStream."""
         BytesIO.__init__(self)
-        assert isinstance(initial_bytes, bytearray)
         # use the actual object as a buffer, do not copy
-        self._buffer = initial_bytes
+        self._buffer = memoryview(initial_bytes)
+
+    # slighly modified from python2's _pyio
+    def read(self, n=None):
+        """Read from bytestream."""
+        if self.closed:
+            raise ValueError('read from closed file')
+        if n is None:
+            n = -1
+        if not isinstance(n, int):
+            raise TypeError('integer argument expected, got {0!r}'.format(type(n)))
+        if n < 0:
+            n = len(self._buffer)
+        if len(self._buffer) <= self._pos:
+            return b''
+        newpos = min(len(self._buffer), self._pos + n)
+        b = self._buffer[self._pos : newpos]
+        self._pos = newpos
+        # this is necessary for python2, where bytes(memoryview) gives a 'string representation'
+        return bytes(bytearray(b))
